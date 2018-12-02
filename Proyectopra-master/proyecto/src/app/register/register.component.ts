7615f33dd13215ef0309120c20c3aea0413432a9
EneyPineda <mayra_eneydi.1@hotmail.com>
2018-12-01 18:17:52 -0600

    actualizacion

--- Proyectopra-master/proyecto/src/app/register/register.component.ts
+++ Proyectopra-master/proyecto/src/app/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +8,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-
-  constructor() { }
+  public email: string ='';
+  public password: string='';
+  constructor(private router : Router, private authService : AuthService) { }
 
   ngOnInit() {
   }
 
+  onAddUser(){
+    this.authService.registerUser(this.email, this.password)
+    .then((res) =>{
+      this.router.navigate(['/principal']);
+    }).catch(err => console.log('err', err.message));
+  }
 }
--- Proyectopra-master/proyecto/src/app/services/auth.service.ts
+++ Proyectopra-master/proyecto/src/app/services/auth.service.ts
@@ -1,19 +1,26 @@
-import { Injectable } from '@angular/core';
-import { AngularFireAuth } from '@angular/fire/auth';
-import { auth } from 'firebase/app';
-import { map } from 'rxjs/operators';
+import { Injectable } from "@angular/core";
+import { AngularFireAuth } from "@angular/fire/auth";
+import { auth } from "firebase/app";
+import { map } from "rxjs/operators";
 @Injectable()
 export class AuthService {
 
     constructor(public afAuth: AngularFireAuth) { }
 
-    registerUser() { }
+    registerUser(email : string, pass: string) {
+        return new Promise((resolve, reject) => {
+            this.afAuth.auth.createUserWithEmailAndPassword(email, pass)
+                .then(userData => resolve(userData)),
+                err => reject(err);
+        })
+    }
+
     loginEmailUser(email: string, pass: string) {
         return new Promise((resolve, reject) => {
             this.afAuth.auth.signInWithEmailAndPassword(email, pass)
                 .then(userData => resolve(userData),
                     err => reject(err));
-        });
+        })
     }
     loginFacebookUser() {
         return this.afAuth.auth.signInWithPopup(new auth.FacebookAuthProvider());
@@ -26,7 +33,7 @@ export class AuthService {
     }
 
     isAuth() {
-        // tslint:disable-next-line:no-shadowed-variable
-        return this.afAuth.authState.pipe(map( auth => auth));
+                // tslint:disable-next-line:no-shadowed-variable
+        return this.afAuth.authState.pipe(map(auth => auth));
     }
 }
